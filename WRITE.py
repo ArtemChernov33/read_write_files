2669d0c9ab3760b0426a5f2f74880568c806a989
ArtemChernov33 <94698583+ArtemChernov33@users.noreply.github.com>
2022-01-08 20:34:48 +0300

    Add files via upload
    Переделанный файл

--- WRITE.py
+++ WRITE.py
@@ -2,23 +2,30 @@ import os
 from pprint import pprint
 file_list = os.listdir()
 path = os.getcwd()
-list = []
 text = {}
-for file in os.listdir():
-    if file.endswith(".txt"):
-        n = os.path.join(file)
-        text[n] = ''
-        with open(os.path.join(file), encoding='utf-8', ) as infile:
-            t = infile.readlines()
-            #pprint(t)
-        text[n] = len(t), t
-pprint(text)
-a = sorted(text.items(), key=lambda x: x[1])
-#pprint(a)
-with open('d.txt', 'w') as outfile:
-    # for k, v in text.items():
-    #     outfile.write(f'{k}, {v}\n' )
-    # for item in a:
-    #     print(item, file=outfile)
-    print(a, file=outfile, sep="\n")
+def read_files():
+    for file in os.listdir():
+        if file.endswith(".txt"):
+            text[os.path.join(file)] = ''
+            with open(os.path.join(file), encoding='utf-8', ) as infile:
+                count = len(infile.readlines())
+                #print(count)
+                #text[os.path.join(file)] = count
+
+            with open(os.path.join(file), encoding='utf-8', ) as infile:
+                p = infile.read()
+                #pprint(p)
+                print(count)
+                text[os.path.join(file)] = p
+                a = sorted(text.items(), key=lambda x: len(x[1]))
+    #pprint(a)
+    with open('d.txt', 'w') as outfile:
+        for TEXT in a:
+            #print(TEXT)
+            for Text in TEXT:
+                print(Text)
+                outfile.write(f'{Text}\n')
+    #pprint(TEXT)
+
+read_files()
 
